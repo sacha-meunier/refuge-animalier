137a072d8c162d3d209641af56bba6ece0553e9b
Sacha Meunier <sacha.meunier@student.hepl.be>
2025-12-12 10:53:32 +0100

    ✨ feat(): add multi-language support
    Add the support for handling the different languages of the app such as :
    
    - french (fr)
    - english (en)
    - dutch (nl)
    - german (de)

---
+++ lang/nl/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    "failed" => "Deze gegevens komen niet overeen met onze registratie.",
+    "password" => "Het opgegeven wachtwoord is incorrect.",
+    "throttle" =>
+        "Te veel loginpogingen. Probeer het alstublieft over :seconds seconden opnieuw.",
+];
