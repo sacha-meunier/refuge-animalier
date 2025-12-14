7f8e46fd907f1237b18ab8b15a3489fe2a3cc587
Sacha Meunier <sacha.meunier@student.hepl.be>
2025-12-14 13:46:23 +0100

    ♻️ refactor(): code refactoring

--- resources/views/components/tailwind_breakpoints_hint.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="fixed top-2 left-2 z-50 px-2 py-1 text-white text-sm font-bold rounded bg-black/70">
-    <span class="block sm:hidden">XS ( < 640px )</span>
-    <span class="hidden sm:block md:hidden">SM ( ≥ 640px )</span>
-    <span class="hidden md:block lg:hidden">MD ( ≥ 768px )</span>
-    <span class="hidden lg:block xl:hidden">LG ( ≥ 1024px )</span>
-    <span class="hidden xl:block 2xl:hidden">XL ( ≥ 1280px )</span>
-    <span class="hidden 2xl:block">2XL ( ≥ 1536px )</span>
-</div>
