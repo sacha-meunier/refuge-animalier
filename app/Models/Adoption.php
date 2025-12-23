3d516fcc79053a6871b1d27f3d2c81487818ceee
Sacha Meunier <sacha.meunier@student.hepl.be>
2025-12-23 15:30:20 +0100

    ✨ feat(): improve adoption model
    - add belongs to animal relationship
    - add formatted date accessor

--- app/Models/Adoption.php
+++ app/Models/Adoption.php
@@ -2,14 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Adoption extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'username',
         'email',
         'phone',
         'content',
+        'animal_id'
     ];
+
+    public function animal(): BelongsTo
+    {
+        return $this->belongsTo(Animal::class);
+    }
+
+    public function formattedDate(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->created_at?->diffForHumans() ?? __('dates.not_available'),
+        );
+    }
 }
