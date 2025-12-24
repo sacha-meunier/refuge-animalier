8f21c1563d9a217d037e58c2d9198a9ab279d7d5
Sacha Meunier <sacha.meunier@student.hepl.be>
2025-12-24 15:28:46 +0100

    ✨ feat(): add contact page
    - add formatted date method on the model
    - add contact policy
    - add index page for contacts
    - add contact page in sidebar
    - add svg for contact icon in sidebar
    - add contact table component
    - add contact translation strings
    - add contact routes

---
+++ lang/en/pages/contacts/index.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | contacts.index Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+    'title' => 'Contacts',
+    'header_action_label' => 'Add a contact',
+    'action_bar_search_label' => 'Search contacts',
+    'th_name' => 'Name',
+    'th_email' => 'Email',
+    'th_phone' => 'Phone',
+    'th_adoptions' => 'Adoption requests',
+    'th_created_at' => 'Created at',
+];
