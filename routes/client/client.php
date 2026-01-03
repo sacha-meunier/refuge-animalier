e3f8c3a460a1c8c3769b513376bba1a5fc50f6f6
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-01-03 01:34:13 +0100

    ✨ feat(animal / contact / volunteer): init controllers and routes
    Init controllers to be used in routes

--- routes/client/client.php
+++ routes/client/client.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\VolunteerController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([], function () {
+    // Home
     Route::get('/', fn() => view('home'))->name('home');
+
+    // Animals
+    Route::get('/animals', [AnimalController::class, 'index'])->name('animals.index');
+    Route::get('/animals/{animal}', [AnimalController::class, 'show'])->name('animals.show');
+
+    // Contact
+    Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
+
+    // Volunteer
+    Route::get('/volunteer', [VolunteerController::class, 'create'])->name('volunteer.create');
 });
