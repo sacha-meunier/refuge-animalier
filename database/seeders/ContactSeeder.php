a67259b8d3212aa61b089f94a7cf79944c76be60
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-01-03 07:42:32 +0100

    🗃️ db(): add contact seeder

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Load contacts from JSON
+        $contactsData = json_decode(file_get_contents(database_path('data/contacts.json')), true);
+
+        foreach ($contactsData as $contactData) {
+            Contact::create([
+                'username' => $contactData['username'],
+                'email' => $contactData['email'],
+                'phone' => $contactData['phone'],
+            ]);
+        }
+    }
+}
